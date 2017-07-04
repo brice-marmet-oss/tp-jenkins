b14bb866043428e714c0c9f1a831225f227a9149
Nosenthil <brice.marmet@gmail.com>
2017-07-04 10:00:18 +0200

    firt commit

---
+++ src/calculatrice/Add.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatrice;
+
+/**
+ *
+ * @author skjold
+ */
+public class Add {
+    public static String addition(int var1, int var2) {
+        
+        int total = var1 + var2;
+        
+        return "le total de " + var1 + " + " + var2 + " = " + total;
+    }
+}
